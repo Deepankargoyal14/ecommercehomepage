2a4fc515c95a7e414a2cc0819f37f6c2cdae3c20
unknown <dpkg91@gmail.com>
2023-01-25 09:45:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Banner from './components/banner';
+import Header from './components/header';
+import Slider from './components/slider';
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <Slider/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
